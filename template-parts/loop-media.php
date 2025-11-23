a0d1341b5960f8ac5cc073e3708aa826f79f9a60
webbingstudio <mail@webbingstudio.com>
2025-11-23 18:38:20 +0900

    mediaをprojectへ移動

--- template-parts/loop-media.php
+++ template-parts/loop-media.php
@@ -7,35 +7,37 @@
  * @package ws-minimalism
  */
 ?>
-<a class="mi-p-posts__item mi-c-media" href="<?php the_permalink(); ?>">
-	<figure class="mi-c-media__image mi-c-picture">
-		<?php if ( has_post_thumbnail() ) : ?>
-			<?php the_post_thumbnail( 'thumbnail' ); ?>
-		<?php else : ?>
-			<img src="<?php echo esc_url( get_theme_file_uri( 'assets/images/common/noimage.jpg' ) ); ?>" alt="">
-		<?php endif; ?>
-	</figure>
-	<div class="mi-c-media__contents">
-		<?php if ( $post->post_type !== 'page' ) : ?>
-			<time class="mi-c-date" datetime="<?php the_time('c'); ?>"><?php the_time( get_option( 'date_format' ) ); ?></time>
+<article class="mi-p-posts__item mi-p-media">
+	<a class="mi-p-media__inner" href="<?php the_permalink(); ?>">
+		<figure class="mi-p-media__image mi-c-picture">
+			<?php if ( has_post_thumbnail() ) : ?>
+				<?php the_post_thumbnail( 'thumbnail' ); ?>
+			<?php else : ?>
+				<img src="<?php echo esc_url( get_theme_file_uri( 'assets/images/common/noimage.jpg' ) ); ?>" alt="">
+			<?php endif; ?>
+		</figure>
+		<div class="mi-p-media__contents">
+			<?php if ( $post->post_type !== 'page' ) : ?>
+				<time class="mi-c-date" datetime="<?php the_time('c'); ?>"><?php the_time( get_option( 'date_format' ) ); ?></time>
 
-			<?php
-			if ( $post->post_type === 'post' ) :
-				$mi_term = 'category';
-				$mi_post_terms = get_the_terms( $post->ID, $mi_term );
-			?>
-				<?php if ( $mi_post_terms ) : ?>
-				<ul class="mi-p-labels">
-					<?php
-						$mi_post_terms = array_slice( $mi_post_terms, 0, 3 );
-						foreach ( $mi_post_terms as $taxonomy ) :
-					?>
-						<li><span class="mi-c-label mi-c-label--primary mi-c-label--term-<?php esc_html_e( $taxonomy->slug ); ?>"><?php esc_html_e( $taxonomy->name ); ?></span></li>
-					<?php endforeach; ?>
-				</ul>
+				<?php
+				if ( $post->post_type === 'post' ) :
+					$mi_term = 'category';
+					$mi_post_terms = get_the_terms( $post->ID, $mi_term );
+				?>
+					<?php if ( $mi_post_terms ) : ?>
+					<ul class="mi-p-labels">
+						<?php
+							$mi_post_terms = array_slice( $mi_post_terms, 0, 3 );
+							foreach ( $mi_post_terms as $taxonomy ) :
+						?>
+							<li><span class="mi-c-label mi-c-label--primary mi-c-label--term-<?php esc_html_e( $taxonomy->slug ); ?>"><?php esc_html_e( $taxonomy->name ); ?></span></li>
+						<?php endforeach; ?>
+					</ul>
+					<?php endif; ?>
 				<?php endif; ?>
 			<?php endif; ?>
-		<?php endif; ?>
-		<p class="mi-c-title"><?php the_title(); ?></p>
-	</div>
-</a>
+			<p class="mi-c-title"><?php the_title(); ?></p>
+		</div>
+	</a>
+</article>
