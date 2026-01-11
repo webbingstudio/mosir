872d770fb5306ff50e3ea6bb5dba6d91b052cbc2
webbingstudio <mail@webbingstudio.com>
2026-01-11 23:41:26 +0900

    フロントページ用クラスをやめる

--- front-page.php
+++ front-page.php
@@ -14,7 +14,7 @@ get_header();
 // ==============================
 if( have_posts() ) :
 ?>
-<section class="p-home-contents p-post p-post--page">
+<section class="p-post p-post--page">
 	<div class="p-post__contents <?php mo_wp_block_class(); ?>">
 	<?php while( have_posts() ) : the_post(); ?>
 		<?php the_content(); ?>
