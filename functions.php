673cc0ecc0fa591caaf5ec924da88b3947475cae
webbingstudio <mail@webbingstudio.com>
2026-01-28 20:08:14 +0900

    クエリループのリンクに下線を引く

--- functions.php
+++ functions.php
@@ -168,38 +168,4 @@ endif;
 
 
 require_once __DIR__ . '/functions/customizer.php';
-
-
-if ( !function_exists( 'mosi_widgets_init' ) ) {
-	function mosi_widgets_init() {
-		register_sidebar( array(
-			'name' => 'Sidebar (Post)',
-			'id' => 'widget-sidebar-post',
-			'before_widget' => '<div class="widget %2$s">',
-			'after_widget' => '</div>',
-		) );
-
-		register_sidebar( array(
-			'name' => 'Bottom of all content',
-			'id' => 'widget-main',
-			'before_widget' => '<div class="widget %2$s">',
-			'after_widget' => '</div>',
-		) );
-
-		register_sidebar( array(
-			'name' => 'Bottom of post content',
-			'id' => 'widget-main-post',
-			'before_widget' => '<div class="widget %2$s">',
-			'after_widget' => '</div>',
-		) );
-
-		register_sidebar( array(
-			'name' => 'Bottom of drawer',
-			'id' => 'widget-drawer',
-			'before_widget' => '<div class="widget %2$s">',
-			'after_widget' => '</div>',
-		) );
-
-	}
-	add_action( 'widgets_init', 'mosi_widgets_init' );
-}
+require_once __DIR__ . '/functions/widgets.php';
