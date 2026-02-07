b095996297f49452cdf2c14421ff3c627bd78d5e
webbingstudio <mail@webbingstudio.com>
2026-02-07 18:32:21 +0900

    コメントの翻訳

--- comments.php
+++ comments.php
@@ -1,7 +1,6 @@
 <?php
 /**
  * comments.php
- * コメントリスト用テンプレート
  *
  * Referencing the _s theme
  * https://github.com/Automattic/_s/blob/master/comments.php
@@ -9,11 +8,10 @@
  * @package mosir
  */
 
-/*
- * If the current post is protected by a password and
- * the visitor has not yet entered the password we will
- * return early without loading the comments.
- */
+// If the current post is protected by a password and
+// the visitor has not yet entered the password we will
+// return early without loading the comments.
+
 if ( post_password_required() ) {
 	return;
 }
@@ -21,27 +19,20 @@ if ( post_password_required() ) {
 <div id="comments" class="p-comments l-container l-container--sm">
 
 	<?php
-	// You can start editing here -- including this comment!
 	if ( have_comments() ) :
 		?>
 		<p class="p-comments__title c-title c-title--lv3">
-			<?php
-			$_s_comment_count = get_comments_number();
-			if ( '1' === $_s_comment_count ) {
-				printf(
-					/* translators: 1: title. */
-					esc_html__( 'One thought on &ldquo;%1$s&rdquo;', 'mosir' ),
-					'<span class="p-comments__title__label">' . wp_kses_post( get_the_title() ) . '</span>'
-				);
-			} else {
-				printf( 
-					/* translators: 1: comment count number, 2: title. */
-					esc_html( _nx( '%1$s thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', $_s_comment_count, 'comments title', '_s' ) ),
-					number_format_i18n( $_s_comment_count ), // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-					'<span class="p-comments__title__label">' . wp_kses_post( get_the_title() ) . '</span>'
-				);
-			}
-			?>
+		<?php
+			$mosi_comment_count = get_comments_number();
+			// translators: 1: comment count number, 2: title.
+			$mosi_comment_title = (int)$mosi_comment_count === 1 ? __( '%1$s thought on &ldquo;%2$s&rdquo;', 'mosir' ) : __( '%1$s thoughts on &ldquo;%2$s&rdquo;', 'mosir' );
+
+			printf( 
+				esc_html( $mosi_comment_title ),
+				number_format_i18n( $mosi_comment_count ), // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+				'<span class="p-comments__title__label">' . wp_kses_post( get_the_title() ) . '</span>'
+			);
+		?>
 		</p>
 
 		<?php the_comments_navigation(); ?>
@@ -63,7 +54,7 @@ if ( post_password_required() ) {
 		// If comments are closed and there are comments, let's leave a little note, shall we?
 		if ( ! comments_open() ) :
 			?>
-			<p class="p-comments__notice"><?php esc_html_e( 'Comments are closed.', '_s' ); ?></p>
+			<p class="p-comments__notice"><?php echo __( 'Comments are closed.', 'mosir' ); ?></p>
 			<?php
 		endif;
 
