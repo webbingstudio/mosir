669d3fea63574707129847896e132c64cf062910
*lBnnJPGddT <mail@webbingstudio.com>
2026-03-11 20:15:41 +0900

    ヘッダの分離したパーツ削除／変数の整理

--- header.php
+++ header.php
@@ -3,15 +3,20 @@
  * header.php
  *
  * @package mosir
+ *
  */
-
-$mosi_options_header_markup = get_theme_mod( 'mosi_options_header_markup', 'h1' );
-$mosi_options_header_layout = get_theme_mod( 'mosi_options_header_layout', 'large' );
-$mosi_options_drawer_displaying = get_theme_mod( 'mosi_options_drawer_displaying', 'always' );
 
 $mosi_custom_logo_id = get_theme_mod( 'custom_logo' );
 $mosi_custom_logo_src = wp_get_attachment_image_src( $mosi_custom_logo_id, 'full' );
-$mosi_custom_logo_width = isset($mosi_custom_logo_src[1]) ? $mosi_custom_logo_src[1] : (bool)false;
+
+$mosi_args = array(
+	'mosi_options_header_markup' => get_theme_mod( 'mosi_options_header_markup', 'h1' ),
+	'mosi_options_header_layout' => get_theme_mod( 'mosi_options_header_layout', 'large' ),
+	'mosi_options_drawer_displaying' => get_theme_mod( 'mosi_options_drawer_displaying', 'always' ),
+	'mosi_custom_logo_src_url' => isset($mosi_custom_logo_src[0]) ? $mosi_custom_logo_src[0] : (bool)false,
+	'mosi_custom_logo_width' => isset($mosi_custom_logo_src[1]) ? $mosi_custom_logo_src[1] : (bool)false,
+	'mosi_custom_logo_height' => isset($mosi_custom_logo_src[2]) ? $mosi_custom_logo_src[2] : (bool)false
+);
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
@@ -26,136 +31,21 @@ $mosi_custom_logo_width = isset($mosi_custom_logo_src[1]) ? $mosi_custom_logo_sr
 <a class="c-skip" id="skip-link" href="#main-content"><?php echo __( 'Skip to content', 'mosir' ); ?></a>
 
 <?php wp_body_open(); ?>
-<header id="header" class="l-header l-header--<?php echo esc_attr( $mosi_options_header_layout ); ?> l-header--drawer-<?php echo esc_attr( $mosi_options_drawer_displaying ); ?>">
+<header id="header" class="l-header l-header--<?php echo esc_attr( $mosi_args['mosi_options_header_layout'] ); ?> l-header--drawer-<?php echo esc_attr( $mosi_args['mosi_options_drawer_displaying'] ); ?>">
 	<div class="l-header__contents l-container">
 
-		<?php if( is_front_page() && $mosi_options_header_markup === 'h1' ): ?>
-			<h1 class="l-header__siteTitle p-siteTitle">
-		<?php elseif( is_front_page() ): ?>
-			<p class="l-header__siteTitle p-siteTitle">
-		<?php else: ?>
-			<p class="l-header__siteTitle p-siteTitle">
-				<a href="<?php echo esc_url( home_url() ); ?>">
+		<?php get_template_part( 'template-parts/header', 'siteTitle', $mosi_args ); ?>
+
+		<?php if( $mosi_args['mosi_options_header_layout'] !== 'simple' ): ?>
+		<?php get_template_part( 'template-parts/header', 'top', $mosi_args ); ?>
 		<?php endif; ?>
 
-		<?php if( $mosi_custom_logo_src && $mosi_custom_logo_width ): ?>
-			<span class="p-siteTitle__logo"><?php echo '<img src="' . esc_url( $mosi_custom_logo_src[0] ) . '" alt="'. get_bloginfo('name') .'" width="' . $mosi_custom_logo_src[1] . '" height="' . $mosi_custom_logo_src[2] . '">'; ?></span>
-		<?php elseif( $mosi_custom_logo_src ): ?>
-			<span class="p-siteTitle__logo"><?php echo '<img src="' . esc_url( $mosi_custom_logo_src[0] ) . '" alt="'. get_bloginfo('name') .'">'; ?></span>
-		<?php else: ?>
-			<span class="p-siteTitle__label"><?php bloginfo('name'); ?></span>
+		<?php if( $mosi_args['mosi_options_drawer_displaying'] !== 'none' ): ?>
+		<?php get_template_part( 'template-parts/header', 'drawerToggle', $mosi_args ); ?>
 		<?php endif; ?>
 
-		<?php if( is_front_page() && $mosi_options_header_markup === 'h1' ): ?>
-			</h1>
-		<?php elseif( is_front_page() ): ?>
-			</p>
-		<?php else: ?>
-				</a>
-			</p>
-		<?php endif; ?>
-
-		<?php if( $mosi_options_header_layout !== 'simple' ): ?>
-		<div class="l-header__top">
-
-			<?php
-			if( has_nav_menu('header_nav_01') && $mosi_options_header_layout !== 'large' ) {
-				wp_nav_menu(
-					array(
-						'theme_location' => 'header_nav_01',
-						'container'       => 'nav',
-						'container_aria_label'       => 'Global navigation',
-						'container_class' => 'l-header__menu01 p-megaMenu',
-						'menu_id' => 'header-nav-01',
-						'menu_class' => 'menu p-megaMenu__nav c-nav',
-						'link_before'      => '<span class="menu-label c-nav__item__label">',
-						'link_after'      => '</span>',
-					)
-				);
-			}
-			?>
-
-			<?php
-			if( has_nav_menu('header_nav_02') && $mosi_options_header_layout === 'large' ) {
-				wp_nav_menu(
-					array(
-						'theme_location' => 'header_nav_02',
-						'container'       => 'div',
-						'container_class' => 'l-header__menu02 p-horizontalMenu p-horizontalMenu--arrowed',
-						'menu_id' => 'header-nav-02',
-						'menu_class' => 'menu p-horizontalMenu__nav c-nav',
-						'link_before'      => '<span class="menu-label c-nav__item__label">',
-						'link_after'      => '</span>',
-					)
-				);
-			}
-			?>
-
-			<?php
-			if( has_nav_menu('header_nav_03') ) {
-				wp_nav_menu(
-					array(
-						'theme_location' => 'header_nav_03',
-						'container'       => 'div',
-						'container_class' => 'l-header__menu03 p-horizontalMenu',
-						'menu_id' => 'header-nav-03',
-						'menu_class' => 'menu p-horizontalMenu__nav c-nav',
-						'link_before'      => '<span class="menu-label c-nav__item__label">',
-						'link_after'      => '</span>',
-					)
-				);
-			}
-			?>
-		</div>
-		<?php endif; ?>
-
-		<?php if( $mosi_options_drawer_displaying !== 'none' ): ?>
-		<div class="l-header__drawerToggle p-drawerToggle p-drawerToggle--<?php echo esc_attr($mosi_options_drawer_displaying); ?>">
-			<div class="p-drawerToggle__contents l-container">
-				<button aria-label="<?php echo __( 'Open/close drawer menu', 'mosir' ); ?>" id="mosi-drawer-toggle" class="js-mosi-drawer p-drawerToggle__button c-toggleButton<?php echo $mosi_options_header_layout !== 'small' ? ' c-toggleButton--lg' : ''; ?>" data-mosi-drawer-action="toggle" data-mosi-drawer-duration="500" aria-controls="drawer" aria-expanded="false">
-					<span class="c-toggleButton__label">Menu</span>
-					<span class="c-toggleButton__icon"></span>
-				</button>
-			</div>
-		</div>
-		<?php endif; ?>
-
-		<?php if( $mosi_options_header_layout === 'large' ): ?>
-		<div class="l-header__bottom">
-
-			<?php
-			if( has_nav_menu('header_nav_01') ) {
-				wp_nav_menu(
-					array(
-						'theme_location' => 'header_nav_01',
-						'container'       => 'nav',
-						'container_aria_label'       => 'Global navigation',
-						'container_class' => 'l-header__menu01 p-megaMenu p-megaMenu--lg',
-						'menu_id' => 'header-nav-01',
-						'menu_class' => 'menu p-megaMenu__nav c-nav',
-						'link_before'      => '<span class="menu-label c-nav__item__label">',
-						'link_after'      => '</span>',
-					)
-				);
-			}
-			?>
-			<?php
-			if( has_nav_menu('header_nav_04') ) {
-				wp_nav_menu(
-					array(
-						'theme_location' => 'header_nav_04',
-						'container'       => 'div',
-						'container_class' => 'l-header__menu04 p-buttonMenu',
-						'menu_id' => 'header-nav-04',
-						'menu_class' => 'menu p-buttonMenu__nav c-nav',
-						'link_before'      => '<span class="menu-label c-nav__item__label">',
-						'link_after'      => '</span>',
-					)
-				);
-			}
-			?>
-
-		</div>
+		<?php if( $mosi_args['mosi_options_header_layout'] === 'large' ): ?>
+		<?php get_template_part( 'template-parts/header', 'bottom', $mosi_args ); ?>
 		<?php endif; ?>
 	</div>
 </header>
